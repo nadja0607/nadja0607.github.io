ad73c165e7cadb3ac7acdb422c810a3551031fcc
Nadja <nf1108@nyu.edu>
2020-11-12 14:37:55 +0100

    enlarging icons, fixing responsivity

--- sound.js
+++ sound.js
@@ -1,6 +1,14 @@
-function updateScrollPosition() {
+console.log("Hi, this is a testing line");
 
-    var x = window.scrollX;
-    var y = window.scrollY;
+let photo = document.getElementById("icon");
 
-}
+photo.addEventListener("mouseenter", () => {
+    //enlarge the icon on hover
+        photo.style.width = '99%';
+    
+  });
+
+  //specifying what happens when my mouse leaves -icon gets back to its original position
+photo.addEventListener("mouseleave", () => {
+        photo.style.width = '90%';
+  });
