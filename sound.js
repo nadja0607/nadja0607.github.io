e53d43821fe234d354de3b7e8d82550e67a0e1e2
Nadja <nf1108@nyu.edu>
2020-11-12 15:19:52 +0100

    adding the sounds

--- sound.js
+++ sound.js
@@ -1,15 +1,18 @@
 console.log("Hi, this is a testing line");
 
 let photo = document.getElementById("icon");
-photo.style.margintop = '450px';
+photo.style.width = '90%';
 
 photo.addEventListener("mouseenter", () => {
     //enlarge the icon on hover
         photo.style.width = '99%';
-    
   });
 
   //specifying what happens when my mouse leaves -icon gets back to its original position
 photo.addEventListener("mouseleave", () => {
         photo.style.width = '90%';
   });
+  
+  function playAudio(url){
+    new Audio(url).play();
+  }
