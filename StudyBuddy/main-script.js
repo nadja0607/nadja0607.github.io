019ea22ebe441ab0e824b362490deaf26b224e4c
Nadja <nf1108@nyu.edu>
2020-12-03 23:15:58 +0100

    adding the main page

---
+++ StudyBuddy/main-script.js
@@ -0,0 +1,44 @@
+var btn = document.getElementById("activeBtn");
+
+btn.addEventListener("click", function () {
+  if (btn.innerHTML == "Active") {
+    btn.innerHTML = "Not Active";
+    btn.value = "Not Active";
+  } else {
+    btn.innerHTML = "Active";
+    btn.value = "Active";
+  }
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
