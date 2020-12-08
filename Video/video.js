32ee1f51ff33e9cea8f3ab2b71ae5e079fb93898
Nadja <nf1108@nyu.edu>
2020-12-09 00:21:54 +0100

    Adding play/pause option

--- Video/video.js
+++ Video/video.js
@@ -1,32 +1,84 @@
-var myvideo = document.getElementById('myvideo'),
-playbutton = document.getElementById('playme'),
-jumplink = document.getElementById('jump'),
-jumplink2 = document.getElementById('jump2');
+var myvideo = document.getElementById("my_video");
+/* play = document.getElementById("playme");
+jump = document.getElementById("jump");
+jump2 = document.getElementById("jump2");
 
-jumplink.addEventListener("click", function (event) {
-    event.preventDefault();
-    myvideo.play();
-    myvideo.pause();
-    myvideo.currentTime = 20;
-    myvideo.play();
-}, false);
 
-jumplink2.addEventListener("click", function (event) {
-    event.preventDefault();
-    myvideo.play();
-    myvideo.pause();
-    myvideo.currentTime = 135;
-    myvideo.play();
-}, false);
+play.addEventListener("click", () => {
+    timeEnd = 19;
+    console.log("play step 1");
 
-playbutton.addEventListener("click", function () {
-    if (myvideo.paused) {
-        myvideo.play();
-    } else {
+    if (myvideo.currentTime == timeEnd) {
         myvideo.pause();
+     } else {
+         myvideo.play();
+     }
+})
+
+jump.addEventListener("click", () => {
+    timeEnd = 135;
+    console.log("play step 2");
+
+    if (myvideo.currentTime == timeEnd) {
+        myvideo.pause();
+     } else {
+         myvideo.play();
+     }
+})
+
+jump2.addEventListener("click", () => {
+    timeEnd = 220;
+    console.log("play step 3");
+
+    if (myvideo.currentTime == timeEnd) {
+        myvideo.pause();
+     } else {
+         myvideo.play();
+     }
+}) */
+
+var buttons = ['playme','jump','jump2'];
+
+buttons.forEach(function(bn) {
+    document.getElementById(bn).addEventListener(
+        'click', buttonEvents, !1
+    );
+   
+});
+
+function buttonEvents(e) {
+    /* get the id of the clicked button */
+    var element_id = e.target.id;
+    /* E.G. element_id = 'playme', 'jump', or 'jump2' */
+
+    /* declare variables before setting them */
+    var timeStart = 0;
+    var timeEnd = 0;
+
+    /* set start and end values depending 
+       on which button was clicked */
+    switch(element_id) {
+        case 'playme':
+            timeStart = 0;
+            timeEnd = 20; 
+            break;
+        case 'jump':
+            timeStart = 21;
+            timeEnd = 135;
+            break;
+        case 'jump2':
+            timeStart = 136;
+            timeEnd = 220;
     }
-}, false);
 
+    /* call 'playVideo()' */
+    playVideo(timeStart);
+}
 
-/* https://stackoverflow.com/questions/47643091/html5-video-start-video-at-certain-time-and-play-for-x-amount-of-time 
-follow this link to make video responsive on icons - also we want the icons to change color when steps change???*/
+function playVideo(startTime) {
+    /* set video start time */
+    myvideo.currentTime = startTime;
+    /* play video */
+    myvideo.play();
+    
+}
